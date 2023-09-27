9984b0f115dd5272beab49b0725a4349ae219466
kartiksathappan <kartiksathappan@gmail.com>
2023-09-27 11:26:03 -0700

    gitignore updates

--- server/public/wrenchCode.js
+++ server/public/wrenchCode.js
@@ -1,3 +1,15 @@
-(function() { document.querySelectorAll('*').forEach(function(element) {
-  element.style.color = 'blue';
-}); })();
+(function() { // Get all images
+const images = document.getElementsByTagName('img');
+
+// Loop through each image and flip it horizontally
+for (let i = 0; i < images.length; i++) {
+  images[i].style.transform = 'scaleX(-1)';
+}
+
+// Get all elements with text content
+const textElements = document.querySelectorAll('*:not(script):not(link)');
+
+// Loop through each element and make the text color green
+for (let i = 0; i < textElements.length; i++) {
+  textElements[i].style.color = 'green';
+} })();
