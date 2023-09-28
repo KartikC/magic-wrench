eeaab76da1ce7a8a62091f8387a77538ebe43ab3
kartiksathappan <kartiksathappan@gmail.com>
2023-09-28 01:30:57 -0700

    panel working better

--- extension/background.js
+++ extension/background.js
@@ -3,37 +3,15 @@ console.log('Background script running');
 let enabledTabs = new Set();
 
 chrome.action.onClicked.addListener((tab) => {
-  if (enabledTabs.has(tab.id)) {
-    enabledTabs.delete(tab.id);
-    // Send message to content script to remove the panel
-    chrome.tabs.sendMessage(tab.id, {action: 'disable'});
-  } else {
-    enabledTabs.add(tab.id);
-    // Send message to content script to add the panel
-    chrome.tabs.sendMessage(tab.id, {action: 'enable'});
-  }
+  chrome.tabs.sendMessage(tab.id, {action: 'toggle'}, (response) => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+    if (response.status === 'enabled') {
+      enabledTabs.add(tab.id);
+    } else if (response.status === 'disabled') {
+      enabledTabs.delete(tab.id);
+    }
+  });
 });
-
-// function runCommand(command) {
-//     new Function(command)();
-// }
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.action === 'executeCode') {
-//         // Find the active tab to execute the script in
-//         chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-//             const tab = tabs[0];
-//             chrome.scripting.executeScript({
-//                 target: {tabId: tab.id},
-//                 func: runCommand,
-//                 args: [request.code]
-//             }, (result) => {
-//                 if (chrome.runtime.lastError) {
-//                     console.error(chrome.runtime.lastError);
-//                 }
-//             });
-//         });
-//     }
-//     // ... handle other actions
-//   });
-  
